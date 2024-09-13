20db4b8075e6e48084a0ddf9bd9c73d3b2d06d82
Paulo Henrique  ΛP <paulo135657@gmail.com>
2024-09-13 12:59:22 -0300

    let

--- fechamentoCX/src/js/index.js
+++ fechamentoCX/src/js/index.js
@@ -33,11 +33,11 @@ function createRegularInputs() {
         input.className = "etc";
         input.min = "1";
         input.addEventListener("change", function () {
-            formSum()
-        })
+            formSum();
+        });
         document.getElementById("allEtcs").appendChild(input);
     };
-}
+};
 
 function createInputs() {
     for (let i = 0; i < needMoreInputs; i++) {
@@ -47,8 +47,8 @@ function createInputs() {
         input.className = "etc";
         input.min = "1";
         input.addEventListener("change", function () {
-            formSum()
-        })
+            formSum();
+        });
         document.getElementById("allEtcs").appendChild(input);
     };
     isItEmpty = document.querySelectorAll(".etc");
@@ -84,19 +84,19 @@ function formSum() {
         };
 
         if (input.value === "" && !input.id) {
-            control++
-        }
+            control++;
+        };
     }
     );
     document.getElementById("showSum").textContent = "TOTAL:" + sum.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 
-    saveState();
+    saveState();//está criando novos inputs
     updateInput();
-
+    console.log("control é " + control)
     if (control <= 2) {
-        needMoreInputs = 4
-        createInputs()
-    }
+        needMoreInputs = 4;
+        createInputs();
+    };
 };
 function updateInput() {
     let findEmpty;//remove the classList if the element is Empty
@@ -116,7 +116,7 @@ function updateInput() {
     findEmpty = document.querySelectorAll(".becomeDev");
     findEmpty.forEach((input) => {
         if (input.value === "") {
-            input.classList.remove("becomeDev")
+            input.classList.remove("becomeDev");
         };
     });
 
@@ -177,71 +177,69 @@ function jumpToNext() {
     let index = Array.prototype.indexOf.call(nextEl, document.activeElement);
     if (index > -1) {
         let jumpTo = nextEl[index + 1] || nextEl[0];
-        jumpTo.focus()
-    }
-}
+        jumpTo.focus();
+    };
+};
 
 function goToFreeInput() {
     for (let i = 0; i < findEmpty.length; i++) {
         if (findEmpty[i].value === "" && !findEmpty[i].id) {
-            findEmpty[i].focus()
+            findEmpty[i].focus();
             break;
-        }
-    }
-}
+        };
+    };
+};
 
 function findAndClear() {
     isItEmpty.forEach((input) => {
         if (input.value == "" && !input.id) {
-            input.classList.add("hidden")
+            input.classList.add("hidden");
             setTimeout(function () {
-                input.classList.remove("hidden")
+                input.classList.remove("hidden");
             }, 2)
         } else {
             input.type = "text";//put it on middle
             setTimeout(function () {
                 input.type = "number";
             }, 2);
-        }
+        };
 
         if (input.id && input.value === "") {
             input.value = "NÃO";
             setTimeout(function () {
                 input.value = "";
-            }, 2)
+            }, 2);
         };
     });
 
     let sinaisValues = document.getElementsByClassName("becomeSin");
 
     for (let i = 0; i < sinaisValues.length; i++) {
-        const sendToSin = document.createElement("input")
+        const sendToSin = document.createElement("input");
         sendToSin.value = sinaisValues[i].value;
-        sendToSin.tabIndex = "-1";
-        sendToSin.style = " text-align: center; width:55px;font-size: small;"
+        sendToSin.style = " text-align: center; width:55px;font-size: small;";
 
         document.getElementById("sinValues").appendChild(sendToSin);
         setTimeout(function () {
-            sendToSin.remove()
-        })
+            sendToSin.remove();
+        });
     };
 
     let devValues = document.getElementsByClassName("becomeDev");
 
     for (let i = 0; i < devValues.length; i++) {
-        const sendToDev = document.createElement("input")
+        const sendToDev = document.createElement("input");
         sendToDev.value = devValues[i].value;
-        sendToDev.tabIndex = "-1";
-        sendToDev.style = " text-align: center; width:55px;font-size: small;"
+        sendToDev.style = " text-align: center; width:55px;font-size: small;";
 
         document.getElementById("devValues").appendChild(sendToDev);
         setTimeout(function () {
-            sendToDev.remove()
-        })
+            sendToDev.remove();
+        });
     };
 
     //setTimeout volta a pág para o estado anterior
-}
+};
 
 document.addEventListener("keydown", function (event) {
     switch (event.code) {
@@ -249,20 +247,20 @@ document.addEventListener("keydown", function (event) {
             formSum();
             document.getElementById("time").textContent = new Date().toLocaleDateString() + " | " + new Date().toLocaleTimeString();
             findAndClear();
-            document.getElementById("signature").classList.remove("hidden")
+            document.getElementById("signature").classList.remove("hidden");
             window.print();
-            document.getElementById("signature").classList.add("hidden")
+            document.getElementById("signature").classList.add("hidden");
             break;
         case "F8":
             localStorage.clear();
             location.reload();
             break;
         case "KeyT":
-            avulso.focus()
+            avulso.focus();
             break;
         case "KeyL":
             findEmpty = document.querySelectorAll(".etc");
-            goToFreeInput()
+            goToFreeInput();
             break;
         case "KeyD":
             putItOnDevolucoes();
@@ -274,7 +272,7 @@ document.addEventListener("keydown", function (event) {
 
     if (event.key === "Enter") {
         jumpToNext();
-    }
+    };
 });
 
 function saveState() {
@@ -288,19 +286,19 @@ function saveState() {
 
     if (inputs.length > 44) {
         localStorage.setItem("allInputs", inputs.length);
-    }// SOLUCAO TEMPORÁRIA para nn criar novos ao recarregar a pág
-}
+    };// SOLUCAO TEMPORÁRIA para nn criar novos ao recarregar a pág
+};
 
 function loadState() {
     const lastTotalInputs = localStorage.getItem("allInputs");
 
     if (lastTotalInputs) {
         allInputs = parseInt(lastTotalInputs);
-        createRegularInputs()
+        createRegularInputs();
     } else {
-        allInputs = 36
-        createRegularInputs()
-    }
+        allInputs = 36;
+        createRegularInputs();
+    };
 
     const inputs = document.querySelectorAll('.etc');
     inputs.forEach((input, index) => {
@@ -309,11 +307,11 @@ function loadState() {
 
         if (savedValue !== null) {
             input.value = savedValue;
-        }
+        };
 
         if (savedClass !== null) {
             input.className = savedClass;
-        }
+        };
     });
 
     document.getElementById("lastName").textContent += localStorage.getItem("lastUsedName");
@@ -321,5 +319,5 @@ function loadState() {
     document.getElementById("lastLogin").textContent += localStorage.getItem("LastLoginCode");
 
     document.getElementById("lastTime").textContent += localStorage.getItem("time");
-    formSum()
-}
+    formSum();
+};
